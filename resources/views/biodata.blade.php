d61083f307522fd9871526eb58d8013109e66aef
LAPTOP-52TL75LM\ASUS <60900119019@uin-alauddin.ac.id>
2021-10-26 12:09:11 +0700

    praktikum3

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H1>BIODATA</H1>
+    <H3>Nama :   Ulfa Rahman</H3>
+    <H3>Kelas:   B</H3>
+    <H3>Nim  :   60900119019</H3>
+    <H3>Email:   60900119019@uin-alauddin.ac.id</H3>
+    
+</body>
+</html>
